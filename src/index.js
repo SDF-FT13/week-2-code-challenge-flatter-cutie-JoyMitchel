0dbfb5a36efa1aa161c21797e3de8e14f8ee4c8b
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-03-25 09:40:30 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -6,24 +6,32 @@ document.addEventListener("DOMContentLoaded", () => {
             "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
             "votes": 0
         },
+
+
         {
             "id": 2,
             "name": "Mx. Monkey",
             "image": "https://thumbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif",
             "votes": 0
         },
+
+
         {
             "id": 3,
             "name": "Ms. Zebra",
             "image": "https://media2.giphy.com/media/20G9uNqE3K4dRjCppA/source.gif",
             "votes": 0
         },
+
+
         {
             "id": 4,
             "name": "Dr. Lion",
             "image": "http://bestanimations.com/Animals/Mammals/Cats/Lions/animated-lion-gif-11.gif",
             "votes": 0
         },
+
+
         {
             "id": 5,
             "name": "Mme. Panda",
@@ -33,6 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ];
 
     const baseUrl = "http://localhost:3000/characters";
+
 
     const characterBar = document.getElementById("character-bar");
     const characterName = document.getElementById("name");
@@ -72,5 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
         voteCount.textContent = "0";
     });
 });
+
+
 
 
